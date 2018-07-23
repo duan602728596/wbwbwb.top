17faa30fcf23a6d273beafe3fc4d8136924a0f3b
段昊辰 <duanhaochen@126.com>
2018-07-23 20:19:51 +0800

    添加环境变量

--- service/config.js
+++ service/config.js
@@ -1,3 +1,8 @@
+const process = require('process');
+
+const uri = 'http://localhost';
+const port = process.env.NODE_ENV === 'production' ? 5051 : 5050;
+
 module.exports = {
-  apiUri: 'http://localhost:5050'
+  apiUri: `${ uri }:${ port }`
 };
