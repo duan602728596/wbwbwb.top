08286c626d1aef45e9a01b95e571e2f37f1ac513
duan602728596 <duanhaochen@126.com>
2018-10-26 13:11:15 +0800

    调整异步加载reducer的方法

--- src/modules/48Live/Layout.js
+++ src/modules/48Live/Layout.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
-import { hot } from 'react-hot-loader';
+import loadReducer from '../../store/loadReducer';
 import reducer from './store/reducer';
 import Page404 from '../../assembly/404/index';
 import Index from './Index/index';
 import Item from './Item/index';
 
-@hot(module)
+@loadReducer(reducer)
 class ModuleLayout extends Component{
-  static propTypes: Object = {
-    injectReducers: PropTypes.func
-  };
-
-  constructor(): void{
-    super(...arguments);
-
-    this.props.injectReducers && this.props.injectReducers(reducer);
-  }
   render(): React.ChildrenArray<React.Element>{
     return (
       <Switch key="route">
--- src/modules/FriendShip/Layout.js
+++ src/modules/FriendShip/Layout.js
@@ -1,29 +1,16 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
-import { hot } from 'react-hot-loader';
 import Helmet from 'react-helmet';
+import loadReducer from '../../store/loadReducer';
 import reducer from './store/reducer';
 import isGoToLogin from '../../components/isGoToLogin/isGoToLogin';
 import Page404 from '../../assembly/404/index';
 import Index from './Index/index';
 
-@hot(module)
+@loadReducer(reducer)
 @withRouter
 class ModuleLayout extends Component{
-  static propTypes: Object = {
-    history: PropTypes.object,
-    location: PropTypes.object,
-    match: PropTypes.object,
-    injectReducers: PropTypes.func
-  };
-
-  constructor(): void{
-    super(...arguments);
-
-    this.props.injectReducers && this.props.injectReducers(reducer);
-  }
   componentDidMount(): void{
     isGoToLogin.call(this);
   }
