c0cd29bad2b0007f429355b57f34c4c3a0121ca6
段昊辰 <duanhaochen@126.com>
2018-12-03 20:09:36 +0800

    添加友情链接

--- src/assembly/Footer/index.js
+++ src/assembly/Footer/index.js
@@ -8,7 +8,8 @@ class Footer extends Component{
       <Layout.Footer className={ style.footer }>
         <nav className={ style.footerNav }>
           <span>友情链接：</span>
-          <a href="https://bw.lovelyctx.com/" target="_blank" rel="noopener noreferrer">口袋48禁用词汇查询</a>
+          <a href="https://bw.lovelyctx.com" target="_blank" rel="noopener noreferrer">口袋48禁用词汇查询</a>
+          <a href="http://docs.skygrass.club" target="_blank" rel="noopener noreferrer">QQ群黑名单API文档</a>
         </nav>
       </Layout.Footer>
     );
