5cc1c9ac156a8d92f0f7ac539cb09b4726cff6e8
duan602728596 <duanhaochen@126.com>
2018-12-24 12:23:33 +0800

    调整接口

--- service/routers/sso.login.js
+++ service/routers/sso.login.js
@@ -22,10 +22,13 @@ async function login(ctx: Object, next: Function): Promise<void>{
     url: 'https://passport.weibo.cn/sso/login',
     method: 'POST',
     headers: {
-      Referer: 'https://passport.weibo.cn/signin/login?entry=mweibo&r=http%3A%2F%2Fm.weibo.cn',
-      'Content-Type': 'application/x-www-form-urlencoded'
+      Referer: 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=https%3A%2F%2Fm.weibo.cn%2F',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+      Host: 'passport.weibo.cn',
+      Origin: 'https://passport.weibo.cn'
     },
-    data: queryData
+    data: `${ queryData }&r=https://m.weibo.cn/&entry=mweibo&pagerefer=https://m.weibo.cn/login?backURL=https%253A%252F%252Fm.weibo.cn%252F`
   });
 
   // 格式化数据
