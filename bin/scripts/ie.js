10cd799612b4ed6f425602c744e23a970f21d8eb
duan602728596 <duanhaochen@126.com>
2019-01-25 16:49:49 +0800

    修改脚本

--- bin/scripts/ie.js
+++ bin/scripts/ie.js
@@ -1,14 +1,18 @@
 (function () {
   var userAgent = window.navigator.userAgent;
+
   if (/MSIE\s[0-9]{1,2}\.0;/i.test(userAgent)) {
     function handleWindowLoad(event) {
       var ieLv = userAgent.match(/MSIE\s[0-9]{1,2}\.0;/i)[0].match(/[0-9]{1,2}/)[0];
       var element = document.createElement('div');
+
       element.innerHTML = '<p style="font-size: 16px; text-align: center;">你正在使用IE' + ieLv + '浏览器，你当前的浏览器版本过低。</p>'
         + '<p style="font-size: 16px; text-align: center;">为了更好的体验，请升级你的浏览器。</p>';
       element.style.padding = '20px 10px';
+
       document.body.appendChild(element);
       document.getElementById('app').style.display = 'none';
+
       element = null;
       window.detachEvent('onload', handleWindowLoad);
     }
--- bin/scripts/webp.js
+++ bin/scripts/webp.js
@@ -1,7 +1,9 @@
 var isSupportedWebP = isSupportedWebP || false;
+
 (function(){
   var canvas = document.createElement('canvas');
   var image = canvas.toDataURL('image/webp');
+
   isSupportedWebP = image.indexOf('data:image/webp') >= 0;
   canvas = null;
 })();
