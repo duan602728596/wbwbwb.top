44713a79bdeceb7d2a337afdaf5d1c8cf7534481
段昊辰 <duanhaochen@126.com>
2019-02-02 10:52:32 +0800

    调整错误的变量类型

--- src/router/asyncModule.js
+++ src/router/asyncModule.js
@@ -6,7 +6,7 @@ import { injectReducers } from '../store/store';
  * 异步加载、注入模块和reducer
  * @param { Function } loader: 需要异步注入的模块
  */
-function asyncModule(loader: Function): React.Element{
+function asyncModule(loader: Function): Function{
   const Module: Function = lazy(loader);
 
   return (): React.Element => (
