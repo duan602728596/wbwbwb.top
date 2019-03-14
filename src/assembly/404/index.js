f7a9e3c40cae469c7befe2c82d59f41a0580ed29
duan602728596 <duanhaochen@126.com>
2019-03-14 17:55:02 +0800

    refactor: 调整模块

--- src/assembly/404/index.js
+++ src/assembly/404/index.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'antd';
 import publicStyle from '../../components/publicStyle/publicStyle.sass';
 import style from './style.sass';
 
-class Page404 extends Component {
-  render() {
-    return (
-      <div className={ publicStyle.main }>
-        <img className={ style.nvpu } src={ require('./nvpu.jpg') } />
-        <p className={ style.text }>你好像迷路了呢！让我带你回家吧！</p>
-        <div className={ style.center }>
-          <Link to="/">
-            <Button type="danger" icon="rollback" ghost={ true }>我要回家</Button>
-          </Link>
-        </div>
+function Page404() {
+  return (
+    <div className={ publicStyle.main }>
+      <img className={ style.nvpu } src={ require('./nvpu.jpg') } />
+      <p className={ style.text }>你好像迷路了呢！让我带你回家吧！</p>
+      <div className={ style.center }>
+        <Link to="/">
+          <Button type="danger" icon="rollback" ghost={ true }>我要回家</Button>
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Page404;
--- src/assembly/Footer/index.js
+++ src/assembly/Footer/index.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 import style from './style.sass';
 
-class Footer extends Component {
-  render() {
-    return (
-      <Layout.Footer className={ style.footer }>
-        <nav className={ style.footerNav }>
-          <span>友情链接：</span>
-          <a href="https://bw.lovelyctx.com" target="_blank" rel="noopener noreferrer">口袋48禁用词汇查询</a>
-          <a href="http://docs.skygrass.club" target="_blank" rel="noopener noreferrer">QQ群黑名单API文档</a>
-        </nav>
-      </Layout.Footer>
-    );
-  }
+function Footer() {
+  return (
+    <Layout.Footer className={ style.footer }>
+      <nav className={ style.footerNav }>
+        <span>友情链接：</span>
+        <a href="https://bw.lovelyctx.com" target="_blank" rel="noopener noreferrer">口袋48禁用词汇查询</a>
+        <a href="http://docs.skygrass.club" target="_blank" rel="noopener noreferrer">QQ群黑名单API文档</a>
+      </nav>
+    </Layout.Footer>
+  );
 }
 
 export default Footer;
