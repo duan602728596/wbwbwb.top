036fe743f8415595e13bfa1057205ac46eaff1c4
段昊辰 <duanhaochen@126.com>
2019-03-17 15:43:30 +0800

    ci: 调整编译配置

--- .sweetrc.js
+++ .sweetrc.js
@@ -47,7 +47,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: isDevelopment ? '[name].[ext]' : '[hash:5].[ext]',
-          outputPath: 'script/'
+          outputPath: 'dll/'
         }
       }]
     }
